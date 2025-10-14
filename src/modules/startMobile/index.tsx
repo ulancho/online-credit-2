341c308a30a4acae920bf4a17fd8ba9dd0215ed7
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2025-10-14 18:54:21 +0600

    added start info

--- src/modules/startMobile/index.tsx
+++ src/modules/startMobile/index.tsx
@@ -1,22 +1,42 @@
+import { observer } from 'mobx-react-lite';
+import { useEffect } from 'react';
+
+import { useQueryParams } from 'Common/hooks/useQueryParams.ts';
+import { useStartStore } from 'Common/stores/rootStore.tsx';
 import AuthButton from 'Modules/startMobile/components/authButton';
 import PrivacyText from 'Modules/startMobile/components/privacyText';
 
-import styles from './style.module.css';
+import styles from './styles/style.module.css';
 
 const StartMobile = () => {
+  const queryParams = useQueryParams();
+  const startStore = useStartStore();
+
   const handleMBankLogin = () => {
-    console.log('Redirecting to MBANK...');
+    console.log('Redirecting...');
   };
+
+  useEffect(() => {
+    startStore.setQueryParams(queryParams);
+
+    void startStore.fetchStartInfo();
+
+    return () => {
+      startStore.reset();
+    };
+  }, [queryParams, startStore]);
 
   return (
     <main className={styles.mobilePageWrapper}>
       <div className={styles.authContainer}>
         <div className={styles.cardWrapper}>
           <header className={styles.headerSection}>
-            <img src="src/assets/images/ticket_kg.png" alt="partner-logo" className={styles.logo} />
+            {startStore.startInfo?.logoUrl && (
+              <img src={startStore.startInfo.logoUrl} alt="partner-logo" className={styles.logo} />
+            )}
             <div className={styles.titleSection}>
               <h1 className={styles.mainTitle}>Войти через MBANK ID</h1>
-              <p className={styles.subtitle}>Ticket KG</p>
+              <p className={styles.subtitle}>{startStore.startInfo?.clientName}</p>
             </div>
           </header>
 
@@ -27,7 +47,7 @@ const StartMobile = () => {
 
           <footer className={styles.footerSection}>
             <img
-              src="src/assets/icons/mbank-logo-2.svg"
+              src="/src/assets/icons/mbank-logo-2.svg"
               alt="mbank-logo"
               className={styles.footerLogo}
             />
@@ -38,4 +58,4 @@ const StartMobile = () => {
   );
 };
 
-export default StartMobile;
+export default observer(StartMobile);
