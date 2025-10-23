5ed388e8a0fd8ad2740c6db0f7b5b3289ade8a33
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2025-10-23 17:06:16 +0600

    added generated action

--- src/modules/startMobile/index.tsx
+++ src/modules/startMobile/index.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 
+import { Loader } from 'Common/components/loader';
 import { useQueryParams } from 'Common/hooks/useQueryParams.ts';
 import {
   useStartMobileStatusStore,
@@ -12,7 +13,7 @@ import {
 import AuthButton from 'Modules/startMobile/components/authButton';
 import PrivacyText from 'Modules/startMobile/components/privacyText';
 
-import styles from './styles/style.module.css';
+import styles from './styles/style.module.scss';
 
 const StartMobile = () => {
   const queryParams = useQueryParams();
@@ -61,6 +62,52 @@ const StartMobile = () => {
     };
   }, [redirectUrl, startMobileStatusStore, status]);
 
+  const renderDefaultContent = () => {
+    return (
+      <section className={styles.actionSection}>
+        {shouldShowSkeleton ? (
+          <Skeleton height={56} borderRadius={12} className={styles.buttonSkeleton} />
+        ) : (
+          <AuthButton
+            onClick={handleMBankLogin}
+            isLoading={startMobileInfoStore.isLoadingStartMobile}
+          >
+            Перейти в MBANK
+          </AuthButton>
+        )}
+        {shouldShowSkeleton ? (
+          <div className={styles.privacySkeleton}>
+            <Skeleton height={12} />
+            <Skeleton height={12} />
+            <Skeleton height={12} width="80%" />
+          </div>
+        ) : (
+          <PrivacyText />
+        )}
+      </section>
+    );
+  };
+
+  const renderContainerContent = () => {
+    switch (status) {
+      case 'GENERATED':
+        return (
+          <div className={styles.generatedContainer}>
+            <h2 className={styles.mainTitle}>
+              Ждем подтверждения <br />
+              входа
+            </h2>
+            <p className={styles.subtitle}>Это займёт всего несколько секунд</p>
+            <Loader classes={styles.loader} />
+          </div>
+        );
+      default:
+        return renderDefaultContent();
+    }
+  };
+
+  console.log('status: ', status);
+
   return (
     <main className={styles.mobilePageWrapper}>
       <div className={styles.authContainer}>
@@ -71,38 +118,18 @@ const StartMobile = () => {
             ) : (
               logoUrl && <img src={logoUrl} alt="partner-logo" className={styles.logo} />
             )}
-            <div className={styles.titleSection}>
-              <h1 className={styles.mainTitle}>Войти через MBANK ID</h1>
-              {shouldShowSkeleton ? (
-                <Skeleton height={22} width="60%" className={styles.subtitleSkeleton} />
-              ) : (
-                <p className={styles.subtitle}>{clientName}</p>
-              )}
-            </div>
-          </header>
-
-          <section className={styles.actionSection}>
-            {shouldShowSkeleton ? (
-              <Skeleton height={56} borderRadius={12} className={styles.buttonSkeleton} />
-            ) : (
-              <AuthButton
-                onClick={handleMBankLogin}
-                isLoading={startMobileInfoStore.isLoadingStartMobile}
-              >
-                Перейти в MBANK
-              </AuthButton>
-            )}
-            {shouldShowSkeleton ? (
-              <div className={styles.privacySkeleton}>
-                <Skeleton height={12} />
-                <Skeleton height={12} />
-                <Skeleton height={12} width="80%" />
+            {!status && (
+              <div className={styles.titleSection}>
+                <h1 className={styles.mainTitle}>Войти через MBANK ID</h1>
+                {shouldShowSkeleton ? (
+                  <Skeleton height={22} width="60%" className={styles.subtitleSkeleton} />
+                ) : (
+                  <p className={styles.subtitle}>{clientName}</p>
+                )}
               </div>
-            ) : (
-              <PrivacyText />
             )}
-          </section>
-
+          </header>
+          {renderContainerContent()}
           <footer className={styles.footerSection}>
             <img
               src="/src/assets/icons/mbank-logo-2.svg"
