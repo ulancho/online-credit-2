ec67e8a3a7c475e1a9bfa1f08d82694822d5da06
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2025-10-23 13:19:24 +0600

    turned on skeleton animation

--- src/modules/startMobile/index.tsx
+++ src/modules/startMobile/index.tsx
@@ -1,8 +1,7 @@
 import { observer } from 'mobx-react-lite';
 import { useEffect } from 'react';
-import 'react-loading-skeleton/dist/skeleton.css';
-
 import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 import { useQueryParams } from 'Common/hooks/useQueryParams.ts';
 import {
