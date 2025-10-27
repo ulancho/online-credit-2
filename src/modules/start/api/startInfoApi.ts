fe1fc5966527c6c95bf53e8f54ccab5e7fd128a6
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2025-10-27 19:57:27 +0600

    changed preprod path

--- src/modules/start/api/startInfoApi.ts
+++ src/modules/start/api/startInfoApi.ts
@@ -25,9 +25,7 @@ export interface StartInfoResponse {
 }
 
 const startInfoClient = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_BASE_URL ??
-    'https://preprodib.mbank.kg/svc-biz-ib-cbk-mbank-id-auth/v1/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? 'https://mbank-idtest.cbk.kg/oauth2/v1',
   headers: {
     accept: 'application/json',
     'Content-Type': 'application/json',
