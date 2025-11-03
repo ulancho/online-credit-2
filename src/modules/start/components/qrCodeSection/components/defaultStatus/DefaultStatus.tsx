98da0da238410b4bbf39f41de592ebccf2a504ad
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2025-11-03 11:42:16 +0600

    fixed qr placeholder

--- src/modules/start/components/qrCodeSection/components/defaultStatus/DefaultStatus.tsx
+++ src/modules/start/components/qrCodeSection/components/defaultStatus/DefaultStatus.tsx
@@ -26,11 +26,11 @@ export const DefaultStatus = ({
     <div className={styles.wrapper}>
       <div className={styles.qrCodeWrapper}>
         <div ref={qrContainerRef} className={styles.qrCanvas} aria-hidden="true" />
-        {!qrReady && <div className={styles.qrPlaceholder}>{placeholderText}</div>}
       </div>
       <div className={qrError ? styles.errorMessage : styles.timerSection}>
         <span className={qrError ? styles.errorText : styles.timerText}>{timerLabel}</span>
       </div>
+      {!qrReady && <div className={styles.qrPlaceholder}>{placeholderText}</div>}
     </div>
   );
 };
