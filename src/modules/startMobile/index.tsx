a0177f44deb4666ea23dddf859d81f35db805995
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2025-11-05 16:06:32 +0600

    refactoring

--- src/modules/startMobile/index.tsx
+++ src/modules/startMobile/index.tsx
@@ -1,21 +1,19 @@
 import { observer } from 'mobx-react-lite';
 import { useEffect } from 'react';
-import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 
-import confirmedIconUrl from 'Assets/icons/confirmed.svg';
-import deniedMobileIconUrl from 'Assets/icons/denied-mobile.svg';
-import mbankLogoUrl from 'Assets/icons/mbank-logo-2.svg';
-import { Loader } from 'Common/components/loader';
-import ReloadButton from 'Common/components/reloadButton';
 import { useQueryParams } from 'Common/hooks/useQueryParams.ts';
 import {
   useStartMobileStatusStore,
   useStartMobileStore,
   useStartStore,
 } from 'Common/stores/rootStore.tsx';
-import AuthButton from 'Modules/startMobile/components/authButton';
-import PrivacyText from 'Modules/startMobile/components/privacyText';
+import ConfirmedStatus from 'Modules/startMobile/components/confirmedStatus/ConfirmedStatus.tsx';
+import DefaultContent from 'Modules/startMobile/components/defaultContent/DefaultContent.tsx';
+import DeniedStatus from 'Modules/startMobile/components/deniedStatus/DeniedStatus.tsx';
+import Footer from 'Modules/startMobile/components/footer/Footer.tsx';
+import GeneratedStatus from 'Modules/startMobile/components/generatedStatus/GeneratedStatus.tsx';
+import Header from 'Modules/startMobile/components/header/Header.tsx';
 
 import styles from './styles/style.module.scss';
 
@@ -75,66 +73,22 @@ const StartMobile = () => {
     };
   }, [redirectUrl, startMobileStatusStore, status]);
 
-  const renderDefaultContent = () => {
-    return (
-      <section className={styles.actionSection}>
-        {shouldShowSkeleton ? (
-          <Skeleton height={56} borderRadius={12} className={styles.buttonSkeleton} />
-        ) : (
-          <AuthButton
-            onClick={handleMBankLogin}
-            isLoading={startMobileInfoStore.isLoadingStartMobile}
-          >
-            Перейти в MBANK
-          </AuthButton>
-        )}
-        {shouldShowSkeleton ? (
-          <div className={styles.privacySkeleton}>
-            <Skeleton height={12} />
-            <Skeleton height={12} />
-            <Skeleton height={12} width="80%" />
-          </div>
-        ) : (
-          <PrivacyText />
-        )}
-      </section>
-    );
-  };
-
   const renderContainerContent = () => {
     switch (status) {
       case 'GENERATED':
-        return (
-          <div className={styles.generatedContainer}>
-            <h2 className={styles.mainTitle}>
-              Ждем подтверждения <br />
-              входа
-            </h2>
-            <p className={styles.subtitle}>Это займёт всего несколько секунд</p>
-            <Loader classes={styles.loader} />
-          </div>
-        );
+        return <GeneratedStatus />;
       case 'CONFIRMED':
-        return (
-          <div className={styles.confirmedContainer}>
-            <h2 className={styles.mainTitle}>
-              Вы успешно вошли через <br /> MBank ID
-            </h2>
-            <p className={styles.subtitle}>Сейчас перенаправим вас в сервис..</p>
-            <img src={confirmedIconUrl} alt="confirmed" />
-          </div>
-        );
+        return <ConfirmedStatus />;
       case 'DENIED':
-        return (
-          <div className={styles.deniedContainer}>
-            <h2 className={styles.mainTitle}>Доступ отклонён</h2>
-            <p className={styles.subtitle}>Если это было случайно — попробуйте войти снова.</p>
-            <img src={deniedMobileIconUrl} alt="denied" className={styles.deniedLogo} />
-            <ReloadButton onClick={handleReload} className={styles.deniedBtn} />
-          </div>
-        );
+        return <DeniedStatus onReload={handleReload} />;
       default:
-        return renderDefaultContent();
+        return (
+          <DefaultContent
+            shouldShowSkeleton={shouldShowSkeleton}
+            onLogin={handleMBankLogin}
+            isLoading={startMobileInfoStore.isLoadingStartMobile}
+          />
+        );
     }
   };
 
@@ -142,27 +96,14 @@ const StartMobile = () => {
     <main className={styles.mobilePageWrapper}>
       <div className={styles.authContainer}>
         <div className={styles.cardWrapper}>
-          <header className={styles.headerSection}>
-            {shouldShowSkeleton ? (
-              <Skeleton circle height={80} width={80} containerClassName={styles.logoSkeleton} />
-            ) : (
-              logoUrl && <img src={logoUrl} alt="partner-logo" className={styles.logo} />
-            )}
-            {!status && (
-              <div className={styles.titleSection}>
-                <h1 className={styles.mainTitle}>Войти через MBANK ID</h1>
-                {shouldShowSkeleton ? (
-                  <Skeleton height={22} width="60%" className={styles.subtitleSkeleton} />
-                ) : (
-                  <p className={styles.subtitle}>{clientName}</p>
-                )}
-              </div>
-            )}
-          </header>
+          <Header
+            shouldShowSkeleton={shouldShowSkeleton}
+            logoUrl={logoUrl}
+            status={status}
+            clientName={clientName}
+          />
           {renderContainerContent()}
-          <footer className={styles.footerSection}>
-            <img src={mbankLogoUrl} alt="mbank-logo" className={styles.footerLogo} />
-          </footer>
+          <Footer />
         </div>
       </div>
     </main>
