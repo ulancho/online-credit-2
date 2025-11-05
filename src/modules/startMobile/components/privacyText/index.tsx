798828746ad976729c2ac63d26f733197e525231
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2025-11-05 17:15:21 +0600

    added privacy policy url

--- src/modules/startMobile/components/privacyText/index.tsx
+++ src/modules/startMobile/components/privacyText/index.tsx
@@ -1,15 +1,28 @@
+import { observer } from 'mobx-react-lite';
+
+import { useStartStore } from 'Common/stores/rootStore.tsx';
+
 import styles from './PrivacyText.module.css';
 
 function PrivacyText() {
+  const startService = useStartStore();
+  const privacyPolicyUrl = startService.startInfo?.privacyPolicyUrl;
+  const termOfServiceUrl = startService.startInfo?.termOfServiceUrl;
+
   return (
     <div className={styles.privacyContainer}>
       <p className={styles.privacyText}>
         Прежде чем начать работу с приложением &quot;Ticket&quot;, вы можете ознакомиться с его{' '}
-        <a className={styles.privacyLink}>политикой конфиденциальности</a> и{' '}
-        <a className={styles.privacyLink}>условиями пользования.</a>
+        <a target="_blank" href={privacyPolicyUrl} className={styles.privacyLink} rel="noreferrer">
+          политикой конфиденциальности
+        </a>{' '}
+        и{' '}
+        <a target="_blank" href={termOfServiceUrl} className={styles.privacyLink} rel="noreferrer">
+          условиями пользования.
+        </a>
       </p>
     </div>
   );
 }
 
-export default PrivacyText;
+export default observer(PrivacyText);
