5e50186165a04b3f30bc3ba5f51ddac721b4e28b
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2025-11-12 14:45:25 +0600

    added switch language

--- src/common/components/languageSwitcher/LanguageSwitcher.tsx
+++ src/common/components/languageSwitcher/LanguageSwitcher.tsx
@@ -1,25 +1,40 @@
+import classNames from 'classnames';
+
 import kg from 'Assets/icons/countries/kg.svg';
 import ru from 'Assets/icons/countries/ru.svg';
 import en from 'Assets/icons/countries/us.svg';
+import { useTranslation } from 'Common/i18n';
 
 import styles from './LanguageSwitcher.module.scss';
 
+const flags: Record<string, string> = {
+  kg,
+  ru,
+  en,
+};
+
 const LanguageSwitcher = () => {
+  const { language, languages, setLanguage } = useTranslation();
+
   return (
     <div className={styles.container}>
       <ul>
-        <li>
-          <img src={kg} alt="kg" />
-          <span>KG</span>
-        </li>
-        <li>
-          <img src={ru} alt="ru" />
-          <span>RU</span>
-        </li>
-        <li>
-          <img src={en} alt="en" />
-          <span>EN</span>
-        </li>
+        {languages.map((item) => {
+          const isActive = item.code === language;
+          return (
+            <li
+              key={item.code}
+              className={classNames(isActive && styles.active)}
+              role="option"
+              onClick={() => {
+                setLanguage(item.code);
+              }}
+            >
+              <img src={flags[item.code]} alt={item.code} />
+              <span className={styles.optionLabel}>{item.label}</span>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
