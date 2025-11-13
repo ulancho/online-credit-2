c8013dc97a62fad0b8f5f36338f30b921eac7343
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2025-11-13 13:58:39 +0600

    added localization in the header

--- src/modules/startDesktop/components/qrCodeSection/components/header/Header.tsx
+++ src/modules/startDesktop/components/qrCodeSection/components/header/Header.tsx
@@ -1,11 +1,13 @@
+import { useTranslation } from 'Common/i18n';
+
 import styles from './Header.module.scss';
 
 export const Header = () => {
+  const { t } = useTranslation();
+
   return (
     <header className={styles.header}>
-      <h2 className={styles.title}>
-        Наведите QR-сканер <br /> из приложения MBANK
-      </h2>
+      <h2 className={styles.title}>{t('startDesktop.qr.header.title')}</h2>
     </header>
   );
 };
