dbb24337de9f170aa250c90672c84aa34ebcf69d
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2025-11-17 17:28:02 +0600

    added localization generated state

--- src/modules/startMobile/components/generatedStatus/GeneratedStatus.tsx
+++ src/modules/startMobile/components/generatedStatus/GeneratedStatus.tsx
@@ -1,15 +1,15 @@
 import { Loader } from 'Common/components/loader';
+import { useTranslation } from 'Common/i18n';
 
 import styles from './GeneratedStatus.module.scss';
 
 const GeneratedStatus = () => {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.generatedContainer}>
-      <h2 className={styles.mainTitle}>
-        Ждем подтверждения <br />
-        входа
-      </h2>
-      <p className={styles.subtitle}>Это займёт всего несколько секунд</p>
+      <h2 className={styles.mainTitle}>{t('startMobile.generated.title')}</h2>
+      <p className={styles.subtitle}>{t('startMobile.generated.subtitle')}</p>
       <Loader classes={styles.loader} />
     </div>
   );
