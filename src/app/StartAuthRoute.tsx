8ff7390727817c9978778583e5fadff2fe9e0cb6
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2025-11-18 16:22:41 +0600

    fixed start mobile route

--- src/app/StartAuthRoute.tsx
+++ src/app/StartAuthRoute.tsx
@@ -1,13 +1,21 @@
 import { useMemo } from 'react';
 
+import LanguageSwitcher from 'Common/components/languageSwitcher/LanguageSwitcher.tsx';
 import { isMobileUserAgent } from 'Common/utils/isMobileUserAgent.ts';
 import StartDesktop from 'Modules/startDesktop';
 import StartMobile from 'Modules/startMobile';
 
+const StartMobileRoute = () => (
+  <>
+    <StartMobile />
+    <LanguageSwitcher />
+  </>
+);
+
 function StartAuthRoute() {
   const isMobile = useMemo(() => isMobileUserAgent(), []);
 
-  return isMobile ? <StartMobile /> : <StartDesktop />;
+  return isMobile ? <StartMobileRoute /> : <StartDesktop />;
 }
 
 export default StartAuthRoute;
