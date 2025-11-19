0323c0ae90b14957dae586b46411351122e7bd59
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2025-11-19 11:49:42 +0600

    added localization for error page

--- src/modules/errorDesktop/components/leftSection/index.tsx
+++ src/modules/errorDesktop/components/leftSection/index.tsx
@@ -1,13 +1,15 @@
+import { useTranslation } from 'Common/i18n';
+
 import styles from './style.module.scss';
 
 export const LeftSection = function () {
+  const { t } = useTranslation();
+
   return (
     <section className={styles.leftSection}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Вход через MBank ID временно недоступен для этого запроса</h1>
-        <p className={styles.subtitle}>
-          Вернитесь на сайт и выберите другой способ авторизации — всё остальное работает как обычно
-        </p>
+        <h1 className={styles.title}>{t('errorDesktop.left.title')}</h1>
+        <p className={styles.subtitle}>{t('errorDesktop.left.subtitle')}</p>
       </div>
     </section>
   );
