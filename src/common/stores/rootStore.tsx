3f572b59b1a9ba3d8af21c0bbafa25db0dc8d8a7
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2026-03-03 16:53:28 +0600

    cleared store

--- src/common/stores/rootStore.tsx
+++ src/common/stores/rootStore.tsx
@@ -2,26 +2,9 @@ import { toJS } from 'mobx';
 import { createContext, useContext, type ReactNode } from 'react';
 
 import { QueryParamsService } from 'Common/services/queryParamsService.ts';
-import { CountryCodeService } from 'Modules/startDesktop/services/countryCodeService.ts';
-import { PhoneAuthService } from 'Modules/startDesktop/services/phoneAuthService.ts';
-import { QrInfoService } from 'Modules/startDesktop/services/qrInfoService.ts';
-import { QrStatusService } from 'Modules/startDesktop/services/qrStatusService.ts';
-import { StartInfoService } from 'Modules/startDesktop/services/startInfoService.ts';
-import { MobileInfoService } from 'Modules/startMobile/services/MobileInfoService.ts';
-import { MobileStatusService } from 'Modules/startMobile/services/MobileStatusService.ts';
 
 class RootStore {
-  readonly countryCodesStore = new CountryCodeService();
   readonly queryParamsStore = new QueryParamsService();
-  readonly startStore = new StartInfoService(this.queryParamsStore);
-  readonly qrStore = new QrInfoService(this.queryParamsStore);
-  readonly qrStatusStore = new QrStatusService(this.startStore, this.queryParamsStore);
-  readonly phoneAuthStore = new PhoneAuthService(this.startStore, this.queryParamsStore);
-  readonly startMobileStatusStore = new MobileStatusService(this.queryParamsStore);
-  readonly startMobileStore = new MobileInfoService(
-    this.queryParamsStore,
-    this.startMobileStatusStore,
-  );
 }
 
 const rootStore = new RootStore();
@@ -42,40 +25,6 @@ export function useRootStore() {
   return useContext(RootStoreContext);
 }
 
-export function useCountryCodesStore() {
-  return useRootStore().countryCodesStore;
-}
-
-export function useStartStore() {
-  return useRootStore().startStore;
-}
-
-/**
- ***** /oauth/web/qr  *****
- */
-export function useQrStore() {
-  return useRootStore().qrStore;
-}
-
-/**
- ***** /oauth/web/qr/status  *****
- */
-export function useQrStatusStore() {
-  return useRootStore().qrStatusStore;
-}
-
 export function useQueryParamsStore() {
   return useRootStore().queryParamsStore;
-}
-
-export function usePhoneAuthStore() {
-  return useRootStore().phoneAuthStore;
-}
-
-export function useStartMobileStore() {
-  return useRootStore().startMobileStore;
-}
-
-export function useStartMobileStatusStore() {
-  return useRootStore().startMobileStatusStore;
 }
