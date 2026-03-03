5b1c0a0d14ea694c516ddb859b0f63b4baf6c128
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2026-03-03 15:34:18 +0600

    added css/scss declaration

---
+++ src/vite-env.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="vite/client" />
+declare module '*.module.scss' {
+  const classes: Record<string, string>;
+  export default classes;
+}
+
+declare module '*.module.css' {
+  const classes: Record<string, string>;
+  export default classes;
+}
+
+declare module '*.scss';
+declare module '*.css';
