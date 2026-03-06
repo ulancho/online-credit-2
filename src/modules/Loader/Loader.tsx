6abe46abd7fd4bf99778e6169dcd4dfb3df10e56
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2026-03-06 20:05:46 +0600

    added loader page

--- src/modules/Loader/Loader.tsx
+++ src/modules/Loader/Loader.tsx
@@ -1,30 +1,28 @@
 import Lottie from 'lottie-react';
 
 import loaderAnimation from 'Assets/loader.json';
+import NavBar from 'Common/components/NavBar/NavBar.tsx';
 
 import styles from './Loader.module.scss';
 
 export default function Loader() {
   return (
     <div className={styles.page}>
+      <NavBar />
       <div className={styles.content}>
-        <div className={styles.lottie}>
-          <Lottie animationData={loaderAnimation} loop autoplay />
+        <div className={styles.lottieSection}>
+          <Lottie
+            animationData={loaderAnimation}
+            className={styles.loaderAnimation}
+            loop
+            autoplay
+          />
 
-          <div
-            style={{
-              position: 'absolute',
-              inset: 0,
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              color: '#fff',
-              fontSize: 24,
-              fontWeight: 600,
-            }}
-          >
-            12%
-          </div>
+          <div className={styles.loaderCounter}>12%</div>
+        </div>
+        <div className={styles.textSection}>
+          <h1>Пожалуйста, подождите</h1>
+          <p>Мы очень стараемся обработать вашу заявку как можно быстрее</p>
         </div>
       </div>
     </div>
