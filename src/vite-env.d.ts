12813ec2de0d564357b45e8ba518ae598bd84af7
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2026-03-09 14:10:56 +0600

    added svgr package

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -9,5 +9,13 @@ declare module '*.module.css' {
   export default classes;
 }
 
+declare module '*.svg?react' {
+  import * as React from 'react';
+
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement> & { title?: string }>;
+
+  export default ReactComponent;
+}
+
 declare module '*.scss';
 declare module '*.css';
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import { fileURLToPath, URL } from 'node:url';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 const srcDir = fileURLToPath(new URL('./src', import.meta.url));
 
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       '@': srcDir,
