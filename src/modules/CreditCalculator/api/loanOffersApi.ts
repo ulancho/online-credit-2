430b75a28187269abd5488b2f3685793dd38308d
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2026-03-23 16:35:30 +0600

    fixed documents url

--- src/modules/CreditCalculator/api/loanOffersApi.ts
+++ src/modules/CreditCalculator/api/loanOffersApi.ts
@@ -2,11 +2,9 @@ import { httpClient } from '@/common/api/httpClient';
 
 import type { LoanOfferResponse } from 'Modules/CreditCalculator/models/publicLoanOffer.ts';
 
-const PUBLIC_LOAN_OFFER_API =
-  'https://mbank.cbk.kg/svc-biz-ib-cbk-documents/v1/api/documents/by-code/offer-public-loans';
+const PUBLIC_LOAN_OFFER_API = 'document/by-code/offer-public-loans';
 
-const LOAN_OFFER_API =
-  'https://mbank.cbk.kg/svc-biz-ib-cbk-documents/v1/api/documents/by-code/offer-loans';
+const LOAN_OFFER_API = '/document/by-code/offer-loans';
 
 export async function getPublicLoanOffer(): Promise<LoanOfferResponse> {
   const response = await httpClient.get<LoanOfferResponse>(PUBLIC_LOAN_OFFER_API);
