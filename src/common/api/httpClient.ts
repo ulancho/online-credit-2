97fa7a9f4b6689e4290bb7d39bf41ab02bb46528
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2026-03-24 16:31:37 +0600

    test files

--- src/common/api/httpClient.ts
+++ src/common/api/httpClient.ts
@@ -4,7 +4,7 @@ const baseURL = '/svc-biz-ib-cbk-private-credits/v1/api/webview';
 
 // temporary token implementation
 const token =
-  'ZXlKaGJHY2lPaUpJVXpJMU5pSjkuZXlKcFpDSTZJakF6T1RjME5EQTVMV0ZrTldFdE5EWXdNUzFpTXpSa0xUWTVaREUxTWpjNU9XWmxNQ0lzSW1saGRDSTZNVGMzTkRNME56ZzNPQ3dpWlhod0lqb3hOemMwTXpRNE1UYzRmUS5YVGhlc1IwbTJkd1NjbHNEMWpfbGV6RGNmY1oxdFJJRXlqZEJRci0tbGtv';
+  'ZXlKaGJHY2lPaUpJVXpJMU5pSjkuZXlKcFpDSTZJakF6T1RjME5EQTVMV0ZrTldFdE5EWXdNUzFpTXpSa0xUWTVaREUxTWpjNU9XWmxNQ0lzSW1saGRDSTZNVGMzTkRNME9ESTJPU3dpWlhod0lqb3hOemMwTXpRNE5UWTVmUS5WemRsWjVyTEpQYzltWTN5U2dCNkJCdXpRbUlkQ2t3NnF0Q3hVT3g1N1E4';
 
 export const httpClient = axios.create({
   baseURL,
--- src/modules/CreditCalculator/api/pdfLkdApi.ts
+++ src/modules/CreditCalculator/api/pdfLkdApi.ts
@@ -10,7 +10,9 @@ export type GeneratePdfLkdResponse = {
   pdf_base64: string;
 };
 
-const GENERATE_PDF_LKD_API = '/credit/generate/pdf-lkd';
+// const GENERATE_PDF_LKD_API = '/credit/generate/pdf-lkd';
+const GENERATE_PDF_LKD_API =
+  'https://preprodib.mbank.kg/svc-biz-ib-cbk-private-credits/v1/api/webview/credit/generate/pdf-lkd';
 
 export async function generatePdfLkd(
   payload: GeneratePdfLkdRequest,
