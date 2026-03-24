3e0a0bd11007412b7ada866866f99ae32fe7a625
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2026-03-24 16:25:07 +0600

    test files

--- src/common/api/httpClient.ts
+++ src/common/api/httpClient.ts
@@ -4,7 +4,7 @@ const baseURL = '/svc-biz-ib-cbk-private-credits/v1/api/webview';
 
 // temporary token implementation
 const token =
-  'ZXlKaGJHY2lPaUpJVXpJMU5pSjkuZXlKcFpDSTZJakF6T1RjME5EQTVMV0ZrTldFdE5EWXdNUzFpTXpSa0xUWTVaREUxTWpjNU9XWmxNQ0lzSW1saGRDSTZNVGMzTkRJME1Ua3dOQ3dpWlhod0lqb3hOemMwTWpReU1qQTBmUS4tNVI0NzNUR3BQWlRvcFJqb29vRXBkRXowMGJpX3BXeWhaQkNHdTQ1TE5Z';
+  'ZXlKaGJHY2lPaUpJVXpJMU5pSjkuZXlKcFpDSTZJakF6T1RjME5EQTVMV0ZrTldFdE5EWXdNUzFpTXpSa0xUWTVaREUxTWpjNU9XWmxNQ0lzSW1saGRDSTZNVGMzTkRNME56ZzNPQ3dpWlhod0lqb3hOemMwTXpRNE1UYzRmUS5YVGhlc1IwbTJkd1NjbHNEMWpfbGV6RGNmY1oxdFJJRXlqZEJRci0tbGtv';
 
 export const httpClient = axios.create({
   baseURL,
--- src/modules/Test/Test.tsx
+++ src/modules/Test/Test.tsx
@@ -1,9 +1,10 @@
 import { observer } from 'mobx-react-lite';
 
 import Button from 'Common/components/Button/Button.tsx';
+import { generatePdfLkd } from 'Modules/CreditCalculator/api/pdfLkdApi.ts';
 
 const Test = () => {
-  const handleClick = () => {
+  const handleClick1 = () => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-expect-error
     if (typeof window.deviceReport === 'function') {
@@ -15,9 +16,17 @@ const Test = () => {
     }
   };
 
+  const handleClick2 = () => {};
+
+  const handleClick3 = async () => {
+    await generatePdfLkd({ amount: 150000, termMonths: 12, nominalRate: 12 });
+  };
+
   return (
-    <div>
-      <Button onClick={handleClick}>Test</Button>
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '15px', paddingTop: '15px' }}>
+      <Button onClick={handleClick1}>DeviceReport</Button>
+      <Button onClick={handleClick2}>Loading byte</Button>
+      <Button onClick={handleClick3}>Loading base64</Button>
     </div>
   );
 };
