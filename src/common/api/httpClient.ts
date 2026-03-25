4b40c11afed271cc8f4f25f19d73d64d8bbe368d
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2026-03-25 16:11:21 +0600

    changed token

--- src/common/api/httpClient.ts
+++ src/common/api/httpClient.ts
@@ -4,7 +4,7 @@ const baseURL = '/svc-biz-ib-cbk-private-credits/v1/api/webview';
 
 // temporary token implementation
 const token =
-  'ZXlKaGJHY2lPaUpJVXpJMU5pSjkuZXlKcFpDSTZJakF6T1RjME5EQTVMV0ZrTldFdE5EWXdNUzFpTXpSa0xUWTVaREUxTWpjNU9XWmxNQ0lzSW1saGRDSTZNVGMzTkRNME9ESTJPU3dpWlhod0lqb3hOemMwTXpRNE5UWTVmUS5WemRsWjVyTEpQYzltWTN5U2dCNkJCdXpRbUlkQ2t3NnF0Q3hVT3g1N1E4';
+  'ZXlKaGJHY2lPaUpJVXpJMU5pSjkuZXlKcFpDSTZJamhtWVRNMU9UazBMVEUxTWpjdE5EWXhZaTA1WW1Sa0xXRTFNbUV4TWpJMk1UazJNeUlzSW1saGRDSTZNVGMzTkRNM05ESTNOaXdpWlhod0lqb3hOemMwTXpjME5UYzJmUS5ZSzdOUk84UjhLOGg1SlRoOWxpX2pYX0xPeWVmQ0NtNmFYMVk3QWtZbEpV';
 
 export const httpClient = axios.create({
   baseURL,
