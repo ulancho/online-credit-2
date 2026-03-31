4b012868dcbd075683da1450f055f7fa573af41e
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2026-03-31 19:14:21 +0600

    added log

--- src/modules/CreditCalculator/CreditCalculator.tsx
+++ src/modules/CreditCalculator/CreditCalculator.tsx
@@ -266,7 +266,7 @@ const CreditCalculator = () => {
             <div className={styles.titleBlock}>
               <h1 className={styles.title}>{t('credit-calculator.title')}</h1>
               <p className={styles.description}>{t('credit-calculator.description')}</p>
-              <p>{queryParamsService.token}</p>
+              <p className={styles.description}>{queryParamsService.token}</p>
             </div>
           </div>
           <div className={styles.fieldsSection}>
