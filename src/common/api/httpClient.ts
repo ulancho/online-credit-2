c162d2eedee1b1d8acd4f4b21ba59e60d9391e1f
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2026-04-03 17:39:11 +0600

    changed base url to preprod

--- src/common/api/httpClient.ts
+++ src/common/api/httpClient.ts
@@ -4,8 +4,8 @@ import { applyLanguageInterceptor } from 'Common/api/languageInterceptor.ts';
 import { applyTokenInterceptor } from 'Common/api/tokenInterceptor.ts';
 
 // const baseURL = '/svc-biz-ib-cbk-private-credits/v1/api/webview';
-// const baseURL = 'https://preprodib.mbank.kg/svc-biz-ib-cbk-private-credits/v1/api/webview';
-const baseURL = 'https://mbank.cbk.kg/svc-biz-ib-cbk-private-credits/v1/api/webview';
+const baseURL = 'https://preprodib.mbank.kg/svc-biz-ib-cbk-private-credits/v1/api/webview';
+// const baseURL = 'https://mbank.cbk.kg/svc-biz-ib-cbk-private-credits/v1/api/webview';
 
 export const httpClient = axios.create({
   baseURL,
