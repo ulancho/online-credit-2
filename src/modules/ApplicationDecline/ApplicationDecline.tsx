2726a3500dce57549cd17d93f4213685eba70208
u_zhumabaev <Ulanbek.Zhumabaev@cbk.kg>
2026-04-10 20:12:48 +0600

    committed ExtendedQuestionaire

--- src/modules/ApplicationDecline/ApplicationDecline.tsx
+++ src/modules/ApplicationDecline/ApplicationDecline.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { exitApp } from '@/common/api/common';
-import ExtendedQuestionaire from '@/common/components/ExtendedQuestionaire/ExtendedQuestionaire';
+// import ExtendedQuestionaire from '@/common/components/ExtendedQuestionaire/ExtendedQuestionaire';
 import ConfirmationModal from '@/common/components/Modal/ConfirmationModal';
 import { useTranslation } from '@/common/i18n';
 import { useApplicationStatusStore, useLoanConditionsStore } from '@/common/stores/rootStore';
@@ -108,7 +108,7 @@ export default function ApplicationDecline() {
         </div>
       </header>
       <main className={styles.main}>
-        {isExtended && <ExtendedQuestionaire />}
+        {/*{isExtended && <ExtendedQuestionaire />}*/}
         <h2 className={styles.sectionTitle}>{t('applicationDecline.subTitle')}</h2>
         <div className={styles.accordionList}>
           {DECLINE_REASONS.map((reason) => {
